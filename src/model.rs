7c6829cb37216de88e07a5e03149bfcf52955d2a
MichaelZp <michael.zappe@yahoo.de>
2020-10-09 01:50:04 +0200

    Added fox hole jumps mechanic and rendering. Closes #5

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,7 @@
 use crate::for_fox_sake::read_only_input::ReadOnlyInput;
 use crate::view_models::{LevelViewModel, SpritesViewModel};
 use cgmath;
+use cgmath::InnerSpace;
 pub mod level;
 
 mod fox_hole;
@@ -42,17 +43,31 @@ impl<'a> Model
             sprite_positions[current_index] = hole.exit;
             sprite_tile_map_indices[current_index] = 1;
             current_index = current_index + 1;
+
+            if hole.used
+            {
+                sprite_sizes[current_index] = cgmath::Vector2{ x: 0.15, y: 0.15 };
+                sprite_positions[current_index] = hole.entry;
+                sprite_tile_map_indices[current_index] = 2;
+                current_index = current_index + 1;
+                
+                sprite_sizes[current_index] = cgmath::Vector2{ x: 0.15, y: 0.15 };
+                sprite_positions[current_index] = hole.exit;
+                sprite_tile_map_indices[current_index] = 2;
+                current_index = current_index + 1;
+            }
         }
 
         sprite_sizes[current_index] = cgmath::Vector2{ x: 0.2, y: 0.2 };
         sprite_positions[current_index] = self.player_pos;
         sprite_tile_map_indices[current_index] = 0;
+        current_index = current_index + 1;
 
         SpritesViewModel {
             sprite_sizes: sprite_sizes,
             sprite_positions: sprite_positions,
             sprite_tile_map_indices: sprite_tile_map_indices,
-            sprite_count: 3,
+            sprite_count: current_index as i32,
         }
     }
 
@@ -92,28 +107,54 @@ impl<'a> Model
         })
     }
 
-    pub fn update(&mut self, input: ReadOnlyInput, delta_time: f32)
+    fn move_player(&mut self, input: &ReadOnlyInput, delta_time: f32)
     {
         const SPEED: f32 = 0.2;
 
-        if input.is_input_down(&format!("MoveLeft")) || input.is_input_pressed(&format!("MoveLeft"))
+        if input.is_input_down("MoveLeft") || input.is_input_pressed("MoveLeft")
         {
             self.player_pos.x -= SPEED * delta_time;
         }
         
-        if input.is_input_down(&format!("MoveRight")) || input.is_input_pressed(&format!("MoveRight"))
+        if input.is_input_down("MoveRight") || input.is_input_pressed("MoveRight")
         {
             self.player_pos.x += SPEED * delta_time;
         }
 
-        if input.is_input_down(&format!("MoveDown")) || input.is_input_pressed(&format!("MoveDown"))
+        if input.is_input_down("MoveDown") || input.is_input_pressed("MoveDown")
         {
             self.player_pos.y -= SPEED * delta_time;
         }
 
-        if input.is_input_down(&format!("MoveUp")) || input.is_input_pressed(&format!("MoveUp"))
+        if input.is_input_down("MoveUp") || input.is_input_pressed("MoveUp")
         {
             self.player_pos.y += SPEED * delta_time;
         }
+    }
+
+    fn check_fox_hole_usage(&mut self, input: &ReadOnlyInput)
+    {
+        if input.is_input_down("Use") || input.is_input_pressed("Use")
+        {
+            for hole in &mut self.fox_holes
+            {
+                if hole.used
+                {
+                    continue;
+                }
+
+                if (self.player_pos - hole.entry).magnitude() < 0.1
+                {
+                    self.player_pos = hole.exit;
+                    hole.used = true;
+                }
+            }
+        }
+    }
+
+    pub fn update(&mut self, input: ReadOnlyInput, delta_time: f32)
+    {
+        self.check_fox_hole_usage(&input);
+        self.move_player(&input, delta_time);
     }
 }
